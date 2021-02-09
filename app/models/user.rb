e84522a9124ca2d73163aed2f31725b28e81774b
kensuke1003 <kensukeseven1003@gmail.com>
2021-02-09 06:46:27 +0000

    0209commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  attachment :profile_image #プロフィール画像を保存するためのrefile設定
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-resources :post_images, only: [:new, :create, :index, :show, :destroy]
+resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy] #idの受け渡しがないため、単数形でok
+    
+    resources :post_comments, only: [:create, :destroy] #idを受け渡し、show.htmlが関係しているから複数形である必要があ
+  end
+ resources :users, only: [:show, :edit, :update]
 end
